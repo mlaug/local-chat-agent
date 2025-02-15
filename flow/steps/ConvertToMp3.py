12dc38e510fa34becd532b945d50779d6842227f
Matthias Laug <matthias.laug@gmail.com>
2025-02-15 14:54:32 +0100

    added tmp dir via env variables

--- flow/steps/ConvertToMp3.py
+++ flow/steps/ConvertToMp3.py
@@ -14,7 +14,7 @@ class ConvertToMp3(FlowStep):
         audio = AudioSegment.from_file(io.BytesIO(input_data), format="raw", sample_width=2, frame_rate=16000, channels=1)
 
         # Determine the root directory of the application
-        root_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+        root_dir = os.getenv("TMP_DIR") or os.path.dirname(os.path.abspath(sys.argv[0]))
 
         # Ensure the tmp directory exists in the root directory
         tmp_dir = os.path.join(root_dir, 'tmp')
